c13b2a3486256699082d7662bc22b9ab688c4b6f
Terry Ma <hi@terry.ma>
2026-06-02 08:13:04 -0700

    Use cached accounts endpoint for account lists

--- src/utils/accounts.ts
+++ src/utils/accounts.ts
@@ -14,20 +14,30 @@ export async function listAccountsByConnection(
   snaptrade: Snaptrade,
   user: User,
 ): Promise<AccountsByConnection[]> {
-  const connections = (
-    await snaptrade.connections.listBrokerageAuthorizations(user)
-  ).data;
+  const [connections, accounts] = await Promise.all([
+    snaptrade.connections
+      .listBrokerageAuthorizations(user)
+      .then((response) => response.data),
+    snaptrade.accountInformation
+      .listUserAccounts(user)
+      .then((response) => response.data),
+  ]);
 
-  return Promise.all(
-    connections.map(async (connection) => {
-      const accounts = (
-        await snaptrade.connections.listBrokerageAuthorizationAccounts({
-          ...user,
-          authorizationId: connection.id!,
-        })
-      ).data;
+  const accountsByConnectionId = new Map<string, Account[]>();
+  for (const account of accounts) {
+    const connectionAccounts =
+      accountsByConnectionId.get(account.brokerage_authorization) ?? [];
+    connectionAccounts.push(account);
+    accountsByConnectionId.set(
+      account.brokerage_authorization,
+      connectionAccounts,
+    );
+  }
 
-      return { connection, accounts };
-    }),
-  );
+  return connections.map((connection) => ({
+    connection,
+    accounts: connection.id
+      ? (accountsByConnectionId.get(connection.id) ?? [])
+      : [],
+  }));
 }
